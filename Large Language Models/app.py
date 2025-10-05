b8f20037463f1eb8b575ebbad432374f01eb0817
Faisal Al Azem <35158736+falazem@users.noreply.github.com>
2025-10-04 20:35:53 -0400

    consolidate in one script

--- Large Language Models/app.py
+++ Large Language Models/app.py
@@ -1,6 +1,69 @@
 import streamlit as st
-from streamlit import generate_idea, generate_name
+import pandas as pd
+import numpy as np
+from sklearn.cluster import KMeans
+import os
+from dotenv import load_dotenv
+import cohere
 
+
+load_dotenv()
+co = cohere.ClientV2(os.getenv("API_KEY"))
+
+def generate_idea(industry, temperature):
+    
+    prompt = f"""
+Generate a startup idea given the industry. Return the startup idea and without additional commentary.
+
+Industry: Workplace
+Startup Idea: A platform that generates slide deck contents automatically based on a given outline
+
+Industry: Home Decor
+Startup Idea: An app that calculates the best position of your indoor plants for your apartment
+
+Industry: Healthcare
+Startup Idea: A hearing aid for the elderly that automatically adjusts its levels and with a battery lasting a whole week
+
+Industry: Education
+Startup Idea: An online primary school that lets students mix and match their own curriculum based on their interests and goals
+
+Industry: {industry}
+Startup Idea:"""
+
+    # Call the Cohere Chat endpoint
+    response = co.chat( 
+            messages=[{"role": "user", "content": prompt}],
+            model="command-a-03-2025")
+        
+    return response.message.content[0].text
+
+
+def generate_name(idea, temperature):
+    
+    prompt= f"""
+Generate a startup name given the startup idea. Return the startup name and without additional commentary.
+
+Startup Idea: A platform that generates slide deck contents automatically based on a given outline
+Startup Name: Deckerize
+
+Startup Idea: An app that calculates the best position of your indoor plants for your apartment
+Startup Name: Planteasy 
+
+Startup Idea: A hearing aid for the elderly that automatically adjusts its levels and with a battery lasting a whole week
+Startup Name: Hearspan
+
+Startup Idea: An online primary school that lets students mix and match their own curriculum based on their interests and goals
+Startup Name: Prime Age
+
+Startup Idea: {idea}
+Startup Name:"""
+
+    # Call the Cohere Chat endpoint
+    response = co.chat( 
+            messages=[{"role": "user", "content": prompt}],
+            model="command-a-03-2025")
+        
+    return response.message.content[0].text
 
 st.title("🚀 Startup Idea Generator")
 
