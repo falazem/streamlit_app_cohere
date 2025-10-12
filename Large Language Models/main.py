75c29c2757d5008b524ee80febca09d1c1439cbe
Faisal Al Azem <35158736+falazem@users.noreply.github.com>
2025-10-12 17:06:10 -0400

    initial commit

--- Large Language Models/main.py
+++ Large Language Models/main.py
@@ -9,6 +9,11 @@ from dotenv import load_dotenv
 from rouge_score import rouge_scorer
 from sacrebleu.metrics import BLEU
 import evaluate
+import uuid
+import hnswlib
+from typing import List, Dict
+from unstructured.partition.html import partition_html
+from unstructured.chunking.title import chunk_by_title
 
 # Load environment variables from .env file
 load_dotenv()
@@ -16,45 +21,123 @@ load_dotenv()
 # Setup the Cohere client
 co = cohere.Client(os.getenv("COHERE_API_KEY")) # Get your free API key: https://dashboard.cohere.com/api-keys
 
-#define documents in a list
-
-documents = [
+raw_documents = [
     {
-        "title": "Tall penguins",
-        "text": "Emperor penguins are the tallest."},
+        "title": "Crafting Effective Prompts",
+        "url": "https://docs.cohere.com/docs/crafting-effective-prompts"},
     {
-        "title": "Penguin habitats",
-        "text": "Emperor penguins only live in Antarctica."},
+        "title": "Advanced Prompt Engineering Techniques",
+        "url": "https://docs.cohere.com/docs/advanced-prompt-engineering-techniques"},
     {
-        "title": "What are animals?",
-        "text": "Animals are different from plants."}
+        "title": "Prompt Truncation",
+        "url": "https://docs.cohere.com/docs/prompt-truncation"},
+    {
+        "title": "Preambles",
+        "url": "https://docs.cohere.com/docs/preambles"}
 ]
 
-# Get the user message
-message = "What are the tallest living penguins?"
+class Vectorstore:
+    def __init__(self, raw_documents: List[Dict[str, str]]):
+        self.raw_documents = raw_documents
+        self.docs = []
+        self.docs_embs = []
+        self.retrieve_top_k = 10
+        self.rerank_top_k = 3
+        self.load_and_chunk()
+        self.embed()
+        self.index()
 
-# Generate the response
-response = co.chat_stream(message=message,
-                          documents=documents)
+    def load_and_chunk(self) -> None:
+        """
+        Loads the text from the sources and chunks the HTML content.
+        """
+        print("Loading documents...")
 
-# Display the response
-citations = []
-cited_documents = []
+        for raw_document in self.raw_documents:
+            elements = partition_html(url=raw_document["url"])
+            chunks = chunk_by_title(elements)
+            for chunk in chunks:
+                self.docs.append(
+                    {
+                        "title": raw_document["title"],
+                        "text": str(chunk),
+                        "url": raw_document["url"],
+                    }
+                )
 
-for event in response:
-    if event.event_type == "text-generation":
-        print(event.text, end="")
-    elif event.event_type == "citation-generation":
-        citations.extend(event.citations)
-    elif event.event_type == "stream-end":
-      cited_documents = event.response.documents
+    def embed(self) -> None:
+        """
+        Embeds the document chunks using the Cohere API.
+        """
+        print("Embedding document chunks...")
 
-# Display the citations and source documents
-if citations:
-  print("\n\nCITATIONS:")
-  for citation in citations:
-    print(citation)
+        batch_size = 90
+        self.docs_len = len(self.docs)
+        for i in range(0, self.docs_len, batch_size):
+            batch = self.docs[i : min(i + batch_size, self.docs_len)]
+            texts = [item["text"] for item in batch]
+            docs_embs_batch = co.embed(
+                texts=texts, model="embed-english-v3.0", input_type="search_document", embedding_types=["float"]
+            ).embeddings.float
+            self.docs_embs.extend(docs_embs_batch)
+            
+    def index(self) -> None:
+        """
+        Indexes the document chunks for efficient retrieval.
+        """
+        print("Indexing document chunks...")
 
-  print("\nDOCUMENTS:")
-  for document in cited_documents:
-    print(document)
+        self.idx = hnswlib.Index(space="ip", dim=1024)
+        self.idx.init_index(max_elements=self.docs_len, ef_construction=512, M=64)
+        self.idx.add_items(self.docs_embs, list(range(len(self.docs_embs))))
+
+        print(f"Indexing complete with {self.idx.get_current_count()} document chunks.")
+
+
+    def retrieve(self, query: str) -> List[Dict[str, str]]:
+        """
+        Retrieves document chunks based on the given query.
+
+        Parameters:
+        query (str): The query to retrieve document chunks for.
+
+        Returns:
+        List[Dict[str, str]]: A list of dictionaries representing the retrieved document chunks, with 'title', 'text', and 'url' keys.
+        """
+
+        # Dense retrieval
+        query_emb = co.embed(
+            texts=[query], model="embed-english-v3.0", input_type="search_query", embedding_types=["float"]
+        ).embeddings.float
+        
+        doc_ids = self.idx.knn_query(query_emb, k=self.retrieve_top_k)[0][0]
+
+        # Reranking
+        rank_fields = ["title", "text"] # We'll use the title and text fields for reranking
+
+        docs_to_rerank = [self.docs[doc_id] for doc_id in doc_ids]
+        rerank_results = co.rerank(
+            query=query,
+            documents=docs_to_rerank,
+            top_n=self.rerank_top_k,
+            model="rerank-english-v3.0",
+            rank_fields=rank_fields
+        )
+
+        doc_ids_reranked = [doc_ids[result.index] for result in rerank_results.results]
+
+        docs_retrieved = []
+        for doc_id in doc_ids_reranked:
+            docs_retrieved.append(
+                {
+                    "title": self.docs[doc_id]["title"],
+                    "text": self.docs[doc_id]["text"],
+                    "url": self.docs[doc_id]["url"],
+                }
+            )
+
+        return docs_retrieved
+
+vectorstore = Vectorstore(raw_documents)
+
+print(vectorstore.retrieve("Prompting by giving examples"))
